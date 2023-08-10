8fe49870311ea5493eb5ee51b87ad30911a18950
Josch Kuphal <joschi@tollwerk.de>
2023-08-10 09:57:56 +0200

    Improve status message

--- src/pack/foreign-words-(ChatGPT-prompt)-Ⓣ.js
+++ src/pack/foreign-words-(ChatGPT-prompt)-Ⓣ.js
@@ -7,11 +7,11 @@ import { ErrorInfo, SuccessInfo } from "../lib/info";
         try {
             const text = `Finde alle fremdsprachigen, nicht-deutschen Begriffe in folgendem Text, die nicht bereits in den deutschen Sprachgebrauch eingegangen und quasi eingedeutscht sind, und liste sie gruppiert nach Sprache auf:\n\n${document.body.innerText.replace(/\s+/gi, ' ')}`;
             await navigator.clipboard.writeText(text)
-                .then(res => (new SuccessInfo(d.documentElement, 2, `Copied page text to prompt`, null, stl)).create(true))
-                .catch(res => (new ErrorInfo(d.documentElement, 2, `Failed to copy to prompt`, null, stl)).create(true));
+                .then(res => (new SuccessInfo(d.documentElement, 2, `Copied page text to clipboard (ChatGPT prompt)`, null, stl)).create(true))
+                .catch(res => (new ErrorInfo(d.documentElement, 2, `Failed to copy page text / prompt to clipboard`, null, stl)).create(true));
             ;
         } catch (err) {
-            (new ErrorInfo(d.documentElement, 2, `Failed to copy to prompt`, null, stl)).create(true);
+            (new ErrorInfo(d.documentElement, 2, `Failed to copy page text / prompt to clipboard`, null, stl)).create(true);
         }
     }
 
